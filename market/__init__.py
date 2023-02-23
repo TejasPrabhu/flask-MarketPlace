887fb1f432ef625e70df5373adbd7812cf608cde
TejasPrabhu <tejas.prabhu29@gmail.com>
2023-02-22 19:33:40 -0500

    Add register page and related functionality

--- market/__init__.py
+++ market/__init__.py
@@ -9,7 +9,9 @@ app = Flask(__name__)
 #     os.environ["DB_CONNECTION_STRING"] + "&ssl_ca=/path/to/ca.pem&ssl_verify_cert=true"
 # )
 # app.config["SQLALCHEMY_DATABASE_URI"] = DB_CONNECTION_STRING
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SECRET_KEY"] = "e7f50c78b275c87ae8c27d06"
 
 db = SQLAlchemy(app)
 
